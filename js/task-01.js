70438aaa1d19f933663ca255c8ae5f5988575a94
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-01 00:22:28 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,14 @@
 
 
-const itemEl = document.querySelectorAll('.item');
+const itemEl = document.querySelectorAll('#categories .item');
+
 console.log('Number of categories:',itemEl.length);
+
+itemEl.forEach(element =>{
+    
+    console.log('Category:',element.firstElementChild.innerHTML);
+    console.log("Elements:", element.lastElementChild.children.length)
+});
+
+
+
