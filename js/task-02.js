79689afc7c16e127ecec02ca11a5333237693c05
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-01 19:32:13 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredentsList = document.querySelector('#ingredients');
+console.log(ingredentsList);
+
+const makeIngredientsEl = ingredients.map(el =>{
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.textContent  = el;
+    ingredientsEl.classList.add('item');
+
+    return ingredientsEl;
+});
+
+ingredentsList.append(...makeIngredientsEl);
