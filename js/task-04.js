40b09320239ca51da931e0772b7dc65063af0986
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-02 12:25:09 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,9 @@
+
+const valueEl = document.querySelector('.value');
+const  btnDecrement  =  document.querySelector('button[data-action="decrement"]');
+const  btnIncrement = document.querySelector('button[data-action="increment"]');
+
+
+const counterValue = {
+     value: 0,
+}
