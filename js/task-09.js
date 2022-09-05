f93ffc62837a9d2f6e0afc3e2d4e1bccb8cf62aa
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-05 21:24:57 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,25 @@
+
+// const changeColorBtn = document.querySelector(".change-color");
+// const container = document.querySelector ('.widget');
+// const spanColor = document.querySelector(".color");
+
+const refs = {
+changeColorBtn: document.querySelector(".change-color"),
+container: document.querySelector ('.widget'),
+spanColor: document.querySelector(".color"),
+};
+
+
+refs.changeColorBtn.addEventListener('click',changeColorBack);
+
+function changeColorBack(event) {
+
+  refs.container.style.backgroundColor = getRandomHexColor();
+  refs.spanColor.textContent = refs.container.style.backgroundColor;
+  
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
