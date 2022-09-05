22d4c33bd26483be409a4a3fa805574b517dd537
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-05 18:01:34 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
 
+
+
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+   
+
+    if (form.email.value === "" || form.password.value === "") {
+        return alert("Please fill in all the fields!");
+    };
+
+    // const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+
+    const formEl = event.currentTarget.elements;
+
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+ 
+    const formData = {
+        email,
+        password,
+    }
+    console.log(formData);
+    
+    form.reset();
+};
+
+
+
+
+
+
+
