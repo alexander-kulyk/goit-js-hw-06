02e0c34ac11ff908a49996ae09b69b736ba755ca
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-05 22:56:58 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
+
+
+const container = document.querySelector('#controls');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('.boxes');
+
+
+
+createBtn.addEventListener('click',createBoxes);
+
+function createBoxes(amount) {
+
+};
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
